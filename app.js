0d6b4acc690fcc65ae85d2d77564f28b707e954d
blankCubicle <138397220+blankCubicle@users.noreply.github.com>
2024-06-13 01:44:17 -0400

    Allow user to configure grid size

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const display = document.querySelector('.display');
-const dimensions = [16, 16];
+const sizeOptions = document.querySelector('.grid-size');
+const columns = document.querySelector('#columns');
+const rows = document.querySelector('#rows');
+
+let dimensions = [16, 16];
 
 generateGrid(dimensions);
+sizeOptions.addEventListener('input', applyGridSize);
 display.addEventListener('mouseover', paintSquare);
 
+function applyGridSize(e) {
+  dimensions = [columns.value, rows.value];
+  [columns.nextElementSibling.value, rows.nextElementSibling.value] =
+    dimensions;
+
+  generateGrid(dimensions);
+}
+
 function generateGrid(dimensions) {
-  for (let x = 0; x < dimensions[0]; x++) {
-    for (let y = 0; y < dimensions[1]; y++) {
+  display.innerHTML = '';
+  display.style.setProperty('--columns', dimensions[0]);
+  display.style.setProperty('--rows', dimensions[1]);
+
+  for (let y = 0; y < dimensions[1]; y++) {
+    for (let x = 0; x < dimensions[0]; x++) {
       const square = document.createElement('div');
-      square.dataset.x = x + 1;
-      square.dataset.y = y + 1;
+      square.title = `(${x + 1}, ${y + 1})`;
       display.append(square);
     }
   }
@@ -17,6 +33,7 @@ function generateGrid(dimensions) {
 
 function paintSquare(e) {
   const activeSquare = e.target;
+  if (activeSquare === display) return;
 
   let opacity = parseInt(activeSquare.dataset.opacity) || 0;
   if (opacity < 100) opacity += 10;
